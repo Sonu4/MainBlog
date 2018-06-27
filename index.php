05dcd6c0a74a43cbd19caf2471a8f744540403e5
sonu4 <kotheshubham4@gmail.com>
2018-06-27 12:07:28 +0530

    My Blog Starts From Here

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <nav class="nav-extended">
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo"><i class="material-icons"> </i>Logo</a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="sass.html">Login</a></li>
+              <li><a href="badges.html">Sign Up</a></li>
+              <li><a href="collapsible.html">About Us</a></li>
+            </ul>
+          </div>
+          
+        </nav>
+
+        <ul class="sidenav" id="mobile-demo">
+          <li><a href="sass.html">Login</a></li>
+          <li><a href="badges.html">Sign Up</a></li>
+          <li><a href="collapsible.html">About Us</a></li>
+        </ul>
+        <!--JavaScript at end of body for optimized loading--> 
+     <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+    
+      
+  </html>
